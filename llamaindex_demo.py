3b925ee7faad669c8339ead45f5c9d86bb2a3b27
Bálint Kovács <kovacsb@starschema.net>
2024-07-21 20:10:32 +0200

    Persisting storage
    With previous piece of code, the index is created every time our code is executed. This is inefficient. We only need to create the chunks and the embeddings once, then we can query them as much as we want.
    
    So here with if clause, we check if our `PERSIST_DIR` already exists, and if not, we persist our data in it. And next time, we load our index from that directory.

--- llamaindex_demo.py
+++ llamaindex_demo.py
@@ -1,9 +1,17 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+import logging
+import sys
+import os
+
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    Settings,
+    load_index_from_storage,
+)
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 
-import logging
-import sys
 
 logging.basicConfig(filename="llamalog.txt", level=logging.DEBUG)
 
@@ -13,8 +21,18 @@ Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 # ollama
 Settings.llm = Ollama(model="phi3", request_timeout=360.0)
 
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
+
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
 
 query_engine = index.as_query_engine()
 response = query_engine.query("What did the author do growing up?")
