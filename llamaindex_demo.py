86186d1803eff6c2868f8b50308fbe589e25b686
Bálint Kovács <kovacsb@starschema.net>
2024-07-21 20:02:37 +0200

    Logging added
    Now we are writing Llamaindex Logs in the `llamalog.txt` file. Here, we can see that the HuggingFace repositroy of the embedding model is checked first, then the chunks are created from our document, and lastly, the Ollama API is called to generate the response.

--- llamaindex_demo.py
+++ llamaindex_demo.py
@@ -2,6 +2,11 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 
+import logging
+import sys
+
+logging.basicConfig(filename="llamalog.txt", level=logging.DEBUG)
+
 # bge-base embedding model
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
