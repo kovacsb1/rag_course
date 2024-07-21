f99ca131a8c6bdddbe2007cd62f5372bdaa64644
Bálint Kovács <kovacsb@starschema.net>
2024-07-21 19:55:28 +0200

    Querying our index
    The default language model is OpenAI, so first we load a local one: [Phi3 from Ollama](https://github.com/ollama/ollama). Ollama is a library that enables us to run small language models locally. We set this in the global `Settings` object.
    
    Once our LLM is loaded, we can create a `QueryEngine`. The `QueryEngine` ties all the components together: we can query it, then the embedding model creates an embedding vector of the query, and with that vector, we do a similarity search on the embeddings created from chunks stored in the index. Then the llm creates our response using the most relevant chunks.

--- llamaindex_demo.py
+++ llamaindex_demo.py
@@ -1,8 +1,16 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
 
 # bge-base embedding model
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
+# ollama
+Settings.llm = Ollama(model="phi3", request_timeout=360.0)
+
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
