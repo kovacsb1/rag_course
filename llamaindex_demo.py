846a38053ab78675c649a501e47c3fd4e6b3e31f
Bálint Kovács <kovacsb@starschema.net>
2024-07-21 18:29:04 +0200

    Local embedding model loaded
    Instead of OpenAI, we use a locally running embedding model. We download one from the HuggingFace ML model repository. This embedding model was trained for similarity search.

--- llamaindex_demo.py
+++ llamaindex_demo.py
@@ -1,4 +1,8 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+# bge-base embedding model
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
