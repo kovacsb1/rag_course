e9f3c108d84aa86ad801c558673a39a30fd1308d
Bálint Kovács <kovacsb@starschema.net>
2024-07-23 17:52:35 +0200

    Created a web application using our RAG
    Now we used the chainlit python library to create a simple web app. Chainlit is built on top of streamlit, exclusively to build LLM applications with it. [Read more about chainlit in the docs](https://docs.chainlit.io/get-started/overview).
    
    The `start` function is called when the web app is entered. So here, we set what embedding and language model we are using, load our index and create a query engine from it.
    
    The `main` function, due to the `on_message` decorator, is called only when the user enters a prompt. It calls the query engine to generate a response, and returns this response to the user.

---
+++ rag_frontend.py
@@ -0,0 +1,48 @@
+import chainlit as cl
+from llama_index.core import (
+    StorageContext,
+    load_index_from_storage,
+    Settings
+)
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+PERSIST_DIR = os.environ.get("PERSIST_DIR")
+HF_EMBEDDING_MODEL_NAME = os.environ.get("HF_EMBEDDING_MODEL_NAME")
+OLLAMA_LLM = os.environ.get("OLLAMA_LLM")
+
+SIMILARITY_TOP_K = int(os.environ.get("SIMILARITY_TOP_K"))
+
+
+@cl.on_chat_start
+async def start():
+    # bge-base embedding model
+    Settings.embed_model = HuggingFaceEmbedding(model_name=HF_EMBEDDING_MODEL_NAME)
+
+    # ollama
+    Settings.llm = Ollama(model=OLLAMA_LLM, request_timeout=360.0)
+
+
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+    query_engine = index.as_query_engine(similarity_top_k=SIMILARITY_TOP_K)
+    cl.user_session.set("query_engine", query_engine)
+    
+
+@cl.on_message
+async def main(message: cl.Message):
+    # Your custom logic goes here...
+    query_engine = cl.user_session.get("query_engine")
+
+    response = query_engine.query(message.content)
+
+    # Send a response back to the user
+    await cl.Message(
+        content=response.response,
+    ).send()
