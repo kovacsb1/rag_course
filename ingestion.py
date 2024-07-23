39d4e97a1908437e9520f854803a79a5c3b7418f
Bálint Kovács <kovacsb@starschema.net>
2024-07-23 17:42:24 +0200

    Moved constants to .env file
    Typically, `.env` files are not commited to git, as they can contain sensitive information, and those values are stored in secret managers. Now, we do not have those, this can be thought of as a config file.

--- ingestion.py
+++ ingestion.py
@@ -8,25 +8,33 @@ from llama_index.core.node_parser import SentenceSplitter
 
 import logging
 import os
+from dotenv import load_dotenv
 
 # create log folder
 os.makedirs("logs", exist_ok=True)
-
 logging.basicConfig(filename="logs/ingest_log.txt", level=logging.DEBUG)
 
-PERSIST_DIR = "./storage"
+load_dotenv()
+SOURCE_DIR = os.environ.get("SOURCE_DIR")
+PERSIST_DIR = os.environ.get("PERSIST_DIR")
+
+HF_EMBEDDING_MODEL_NAME = os.environ.get("HF_EMBEDDING_MODEL_NAME")
+
+CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE"))
+CHUNK_OVERLAP = int(os.environ.get("CHUNK_OVERLAP"))
+
 
 # bge-base embedding model
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+Settings.embed_model = HuggingFaceEmbedding(model_name=HF_EMBEDDING_MODEL_NAME)
 
 
 # load the documents and create the index
 splitter = SentenceSplitter(
-    chunk_size=512,
-    chunk_overlap=20,
+    chunk_size=CHUNK_SIZE,
+    chunk_overlap=CHUNK_OVERLAP,
 )
 
-documents = SimpleDirectoryReader("data").load_data()
+documents = SimpleDirectoryReader(SOURCE_DIR).load_data()
 # nodes = splitter.get_nodes_from_documents(documents)
 index = VectorStoreIndex.from_documents(documents, transformations=[splitter])
 # sytore it for later
--- llamaindex_demo.py
+++ llamaindex_demo.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import os
+from dotenv import load_dotenv
 
 from llama_index.core import (
     StorageContext,
@@ -14,20 +15,26 @@ from llama_index.llms.ollama import Ollama
 os.makedirs("logs", exist_ok=True)
 logging.basicConfig(filename="logs/llamalog.txt", level=logging.DEBUG)
 
-PERSIST_DIR = "./storage"
+load_dotenv()
+PERSIST_DIR = os.environ.get("PERSIST_DIR")
+
+HF_EMBEDDING_MODEL_NAME = os.environ.get("HF_EMBEDDING_MODEL_NAME")
+OLLAMA_LLM = os.environ.get("OLLAMA_LLM")
+
+SIMILARITY_TOP_K = int(os.environ.get("SIMILARITY_TOP_K"))
 
 # bge-base embedding model
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+Settings.embed_model = HuggingFaceEmbedding(model_name=HF_EMBEDDING_MODEL_NAME)
 
 # ollama
-Settings.llm = Ollama(model="phi3", request_timeout=360.0)
+Settings.llm = Ollama(model=OLLAMA_LLM, request_timeout=360.0)
 
 
 # load the existing index
 storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
 index = load_index_from_storage(storage_context)
 
-query_engine = index.as_query_engine(similarity_top_k=3)
+query_engine = index.as_query_engine(similarity_top_k=SIMILARITY_TOP_K)
 
 response = query_engine.query("What did the author do growing up?")
 print(response)
