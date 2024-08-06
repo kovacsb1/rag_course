def5b789a83f8c96f027a7c02b391475fcb80ca7
Bálint Kovács <kovacsb@starschema.net>
2024-08-06 18:09:59 +0200

    Using QDrant VectorDB as storage context
    So far, we have used local disk persistance for our embedding vectors.
    
    Now, we use QDrant vector database. QDrant runs on a kubernetes cluster in Azure in our setup. We need to set its URL, port and the collection name we want to create. These are in the `config.toml` file. A vector database creates an effient index, that can quickly find chunks with the most semantic simiarity. However, this effect is only relevant when we have chunks in the range of millions.

--- ingestion.py
+++ ingestion.py
@@ -5,9 +5,12 @@ from llama_index.core import (
 )
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.node_parser import SentenceSplitter
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core import StorageContext
 
 import logging
 import os
+import qdrant_client
 from dotenv import load_dotenv
 import toml
 
@@ -17,12 +20,15 @@ logging.basicConfig(filename="logs/ingest_log.txt", level=logging.DEBUG)
 
 config = toml.load('config.toml')
 SOURCE_DIR = config["persistance"]["source_dir"]
-PERSIST_DIR = config["persistance"]["persist_dir"]
 
 HF_EMBEDDING_MODEL_NAME = config["chunking"]["hf_embedding_model_name"]
 CHUNK_SIZE = config["chunking"]["chunk_size"]
 CHUNK_OVERLAP = config["chunking"]["chunk_overlap"]
 
+QDRANT_URL = config["qdrant"]["url"]
+QDRANT_PORT = config["qdrant"]["port"]
+QDRANT_COLLECTION = config["qdrant"]["collection_name"]
+QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")
 
 # bge-base embedding model
 Settings.embed_model = HuggingFaceEmbedding(model_name=HF_EMBEDDING_MODEL_NAME)
@@ -34,8 +40,22 @@ splitter = SentenceSplitter(
     chunk_overlap=CHUNK_OVERLAP,
 )
 
+# qdrant vectordb client
+vectordb_client = qdrant_client.QdrantClient(
+    url=QDRANT_URL,
+    port=QDRANT_PORT,
+    api_key=QDRANT_API_KEY,
+)
+# qdrant vector store
+vector_store = QdrantVectorStore(client=vectordb_client, collection_name=QDRANT_COLLECTION)
+
+# load documents
 documents = SimpleDirectoryReader(SOURCE_DIR).load_data()
-# nodes = splitter.get_nodes_from_documents(documents)
-index = VectorStoreIndex.from_documents(documents, transformations=[splitter])
-# sytore it for later
-index.storage_context.persist(persist_dir=PERSIST_DIR)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+# here we create the index from the documents, which persists in the vectorDB
+index = VectorStoreIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+    transformations=[splitter]
+)
--- llamaindex_demo.py
+++ llamaindex_demo.py
@@ -2,27 +2,31 @@ import logging
 import sys
 import os
 import toml
+import qdrant_client
 from dotenv import load_dotenv
 
 from llama_index.core import (
-    StorageContext,
-    Settings,
-    load_index_from_storage,
+    VectorStoreIndex,
+    Settings
 )
 
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.vector_stores.qdrant import QdrantVectorStore
 from llama_index.llms.ollama import Ollama
 
 os.makedirs("logs", exist_ok=True)
 logging.basicConfig(filename="logs/llamalog.txt", level=logging.DEBUG)
 
 config = toml.load('config.toml')
-PERSIST_DIR = config["persistance"]["persist_dir"]
-
 HF_EMBEDDING_MODEL_NAME = config["chunking"]["hf_embedding_model_name"]
 SIMILARITY_TOP_K = config["retrieval"]["similarity_top_k"]
 
 OLLAMA_LLM = config["ollama"]["llm"]
+
+QDRANT_URL = config["qdrant"]["url"]
+QDRANT_PORT = config["qdrant"]["port"]
+QDRANT_COLLECTION = config["qdrant"]["collection_name"]
+QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")
 
 # bge-base embedding model
 Settings.embed_model = HuggingFaceEmbedding(model_name=HF_EMBEDDING_MODEL_NAME)
@@ -31,9 +35,15 @@ Settings.embed_model = HuggingFaceEmbedding(model_name=HF_EMBEDDING_MODEL_NAME)
 Settings.llm = Ollama(model=OLLAMA_LLM, request_timeout=360.0)
 
 
-# load the existing index
-storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-index = load_index_from_storage(storage_context)
+# connect to the vectorDB
+vectordb_client = qdrant_client.QdrantClient(
+    url=QDRANT_URL,
+    port=QDRANT_PORT,
+    api_key=QDRANT_API_KEY,
+)
+
+vector_store = QdrantVectorStore(client=vectordb_client, collection_name=QDRANT_COLLECTION)
+index = VectorStoreIndex.from_vector_store(vector_store)
 
 query_engine = index.as_query_engine(similarity_top_k=SIMILARITY_TOP_K)
 
