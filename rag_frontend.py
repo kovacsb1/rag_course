fb9d73041fd0b58091a27783f9c30a2b665a6f29
Bálint Kovács <kovacsb@starschema.net>
2024-08-06 19:01:36 +0200

    OLLama model changed to GPT4o in `rag_frontend.py` as well

--- rag_frontend.py
+++ rag_frontend.py
@@ -4,7 +4,7 @@ from llama_index.core import (
     VectorStoreIndex
 )
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.ollama import Ollama
+from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 
 import os
@@ -12,6 +12,7 @@ import toml
 from dotenv import load_dotenv
 import qdrant_client
 
+load_dotenv()
 
 config = toml.load('config.toml')
 
@@ -19,7 +20,9 @@ HF_EMBEDDING_MODEL_NAME = config["chunking"]["hf_embedding_model_name"]
 SIMILARITY_TOP_K = config["retrieval"]["similarity_top_k"]
 SPARSE_TOP_K = config["retrieval"]["sparse_top_k"]
 
-OLLAMA_LLM = config["ollama"]["llm"]
+AZURE_OPENAI_MODEL= config["openai"]["model"]
+AZURE_DEPLOYMENT_NAME = config["openai"]["azure_deployment"]
+API_VERSION = config["openai"]["api_version"]
 
 QDRANT_URL = config["qdrant"]["url"]
 QDRANT_PORT = config["qdrant"]["port"]
@@ -32,8 +35,13 @@ async def start():
     Settings.embed_model = HuggingFaceEmbedding(model_name=HF_EMBEDDING_MODEL_NAME)
 
     # ollama
-    Settings.llm = Ollama(model=OLLAMA_LLM, request_timeout=360.0)
-
+    Settings.llm = AzureOpenAI(
+        model=AZURE_OPENAI_MODEL,
+        deployment_name=AZURE_DEPLOYMENT_NAME,
+        api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
+        azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT"),
+        api_version=API_VERSION,
+    )
 
     vectordb_client = qdrant_client.QdrantClient(
         url=QDRANT_URL,
